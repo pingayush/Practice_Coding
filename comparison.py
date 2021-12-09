a2fb899494048190dcc0a698d33250c303716100
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-09 09:06:02 +0530

    Create comparison.py

---
+++ comparison.py
@@ -0,0 +1,7 @@
+stock_price_1=[50.23]
+stock_price_2=[75.14]
+print(cmp(stock_price_1,stock_price_2))
+print(cmp(stock_price_1,stock_price_1))
+print(cmp(stock_price_1,stock_price_2))
+print(cmp(stock_price_2,stock_price_1))
+
