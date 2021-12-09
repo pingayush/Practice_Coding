83f09fbd9e342ce47974f5cafcbed9f6a6ab0f04
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-09 09:08:04 +0530

    Create Funcition.py

---
+++ Funcition.py
@@ -0,0 +1,9 @@
+def prime(n):
+    count=0
+    for i in range(1,n+1):
+        if (n%i==0):
+            count=count+1
+    if (count==2):
+            print("Prime Number")
+    else:
+        print("Not a Prime Number")
