029dae06a7c332ad0310d09bceacca1986f52205
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-09 09:07:55 +0530

    Create Funcition 2.py

---
+++ Funcition 2.py
@@ -0,0 +1,17 @@
+def greatest(a,b,c):
+    if a>b:
+        if a>c:
+            print("A is greatest")
+        else:
+            print("C is greatest")
+    elif b>c:
+        print("B is greatest")
+     
+    else:
+        print("C is greatest")
+        
+
+
+greatest(2,3,4)
+greatest(5,8,7)
+greatest(10,2,30)
