e9eb3c70fb7b290a68142863bf69fd07f59faa06
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-09 09:06:17 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,20 @@
+#dictionary is represented by :  {  } i.e.   curly bracket
+d1 = {}
+print(type(d1))
+
+# EXAMPLES::
+
+d2 = {"ayush" : "pizza", "piyush":"burger","anurag":"egg roll"  , "aditya":"chicken" , "ashutosh": "chicken roll" }
+
+print(d2["ayush" ])
+print(d2["piyush"])
+print(d2["anurag"])
+print(d2["aditya"])
+print(d2["ashutosh"])
+
+d2.update({"neeraj":"pasta"} )
+print(d2)
+d2.pop
+print(d2)
+
+
