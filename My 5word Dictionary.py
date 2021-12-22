a054f52011cba8d488a9bd88bf3e3fefc27428e3
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-22 07:28:22 +0530

    Create My 5word Dictionary.py

---
+++ My 5word Dictionary.py
@@ -0,0 +1,7 @@
+d = {"Ineffable": "unspeakable" , "Robotaxi " : "a taxi that is driven without being controlled directly by humans","Knob" : "a round handle","Kingdom":"a country ruled by a king or queen" , "Ash": "the soft grey or black powder that is left after a substance"}
+
+print("WORD")
+a = input()
+
+print("MEANING" )
+print(d[a])
