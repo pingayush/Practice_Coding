33c455f00fb14d0f1f474004df8747af57ffad33
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-22 07:31:24 +0530

    Create Quiz.py

---
+++ Quiz.py
@@ -0,0 +1,8 @@
+while(True):
+    inp = int(input("ENTER YOUR AGE \n"))
+if inp>18:
+    print("YOU CAN FILL UP THIS FORM")
+    breakpoint()
+elif inp<18:
+
+    print(" NOT ALLOWED TO FILL THIS FORM ")
