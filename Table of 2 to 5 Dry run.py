0657944bf04bc6eca366bdb9928116ebdfeb45c2
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-22 07:32:25 +0530

    Create Table of 2 to 5 Dry run.py

---
+++ Table of 2 to 5 Dry run.py
@@ -0,0 +1,80 @@
+for i in range(2,6):
+    print()
+    for j in range(1,11):
+        print(i,"*",j,"=",i*j)
+---------------------------------------
+
+
+
+Dry Run
+
+
+Since i is range (2,6)                          Since j is in range (1,11)
+starting point of i is 2                        starting point of j is 1
+
+2                                               1
+therefore
+
+2*1=2
+2*2=4
+2*3=6
+2*4+8
+2*5=10
+2*6=12
+2*7=14
+2*8=16
+2*9=18
+2*10=20                                      J will stop as the upperlimit of j is 11 which exclusive in the bracket
+
+
+3                                              1
+therfore
+
+3*1=3
+3*2=6
+3*3=9
+3*4+12
+3*5=15
+3*6=18
+3*7=21
+3*8=24
+3*9=27
+3*10=30                                      J will stop as the upperlimit of j is 11 which exclusive in the bracket
+
+
+
+4                                               1
+4*1=4
+4*2=8
+4*3=12
+4*4=16
+4*5=20
+4*6=24
+4*7=28
+4*8=32
+4*9=36
+4*10=40                                      J will stop as the upperlimit of j is 11 which exclusive in the bracket
+
+
+
+
+
+
+5                                               1
+5*1=5
+5*2=4
+5*3=6
+5*4+8
+5*5=10
+5*6=12
+5*7=14
+5*8=16
+5*9=18
+5*10=20                                      J will stop as the upperlimit of j is 11 which exclusive in the bracket
+
+  I will stop as the upperlimit of i is 6 which exclusive in the bracket
+
+
+  Hence the loop breaks
+
+
