02f8e0a6d2b6ab1a3e792d99b0a1ce0e1da2b2b8
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-22 07:32:10 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,19 @@
+mystr = "AYUSH IS IAS"
+
+print(len(mystr))
+
+print(mystr.isalnum())
+
+print(mystr.endswith("IAS"))
+
+print(mystr.count("S"))
+
+print(mystr.find("IAS"))
+
+print(mystr.capitalize())
+
+print(mystr.lower())
+
+print(mystr.upper())
+
+print(mystr.replace("IAS","PCS"))
