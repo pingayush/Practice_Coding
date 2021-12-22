55dfad72004a49eb069760ac54523035561ea778
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-22 07:30:46 +0530

    Create practice @1.py

---
+++ practice @1.py
@@ -0,0 +1,23 @@
+print("Mark1")
+a = int(input())
+
+print("Mark2")
+b = int(input())
+
+print("Mark3")
+c = int(input())
+
+print("Mark4")
+d = int(input())
+
+print("Mark5")
+e = int(input())
+print('AVERAGE MARKS = ')
+
+f=a+b+c+d+e
+print(f/5)
+
+
+
+
+
