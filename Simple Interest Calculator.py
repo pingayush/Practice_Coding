d8b256e55ebaaaf77e5fa6f64a96118b5e5894f1
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-22 07:31:54 +0530

    Create Simple Interest Calculator.py

---
+++ Simple Interest Calculator.py
@@ -0,0 +1,9 @@
+p = int(input('Enter the Principal value: '))
+
+r = int(input('Enter the rate of interest: '))
+
+t = int(input('Tenure: '))
+
+Simple_Interest = (p*r*t)/100
+
+print("\n\nSimple Interest :",int(Simple_Interest))
