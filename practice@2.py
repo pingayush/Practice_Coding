6b7ff77353bb7a9c4165030235daecd919b8903f
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-22 07:30:54 +0530

    Create practice@2.py

---
+++ practice@2.py
@@ -0,0 +1,16 @@
+i=21
+while(i>0):
+    print("WELCOME TO HEIGHT CONVERTER\nName:")
+    name = input()
+    print("ENTER your Height(in Cm)")
+    cm = float(input())
+    print('Height in INCHES =')
+    i = cm / 2.54
+    print(i)
+    print("Height In Foot")
+    f = i / 12
+    print(f)
+    print("\n\n NEXT ONE")
+
+
+
