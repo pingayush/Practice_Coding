023129a60d751ec93d0ded47917be85eff05fe02
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-22 07:31:17 +0530

    Create python list.py

---
+++ python list.py
@@ -0,0 +1,43 @@
+collage = ["IIT","NIT","VIT","BIT" ]
+#print(collage)
+collage.sort()
+#print(collage)
+
+
+numbers = [2,9,6,3,8,1,5,7,4]
+#print(numbers)
+
+#numbers.sort()
+#print(numbers)
+#numbers.reverse()
+#print(numbers)
+'''
+print(max(numbers))
+
+print(numbers)
+print(min(numbers))
+numbers.append(786)
+print(numbers)
+
+numbers.insert(1,420)
+print(numbers)
+
+numbers.remove(9)
+numbers.pop()
+
+numbers[0]=123
+print(numbers)
+
+Mutable=can change 
+Immutable = cannot be change
+
+
+tuple =(1,2,3)
+
+print(tuple)
+'''
+
+a=1
+b=2
+a,b=b,a
+print(a,b)
