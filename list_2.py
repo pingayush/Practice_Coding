8664e6e57cf9c7d272374631782f27cdd8da5ab2
Ayush Kumar Sharma <ayushsharma7403@gmail.com>
2021-12-22 07:27:58 +0530

    Create list_2.py

---
+++ list_2.py
@@ -0,0 +1,29 @@
+#Empty Tuple
+my_tuple = ()
+print(my_tuple)
+
+#Tuple having Integer
+my_tuple_1 = (1,2,3)
+print(my_tuple_1)
+
+#Tuple with mixed database
+my_tuple_2 = (1,"Hello",3,4)
+print(my_tuple_2)
+
+#nested tuple
+my_tuple_3 = ("mouse",[8,4,6],(1,2,3))
+print(my_tuple_3)
+
+#Unpacking
+
+my_tuple = 3,4.6,"dog"
+print(my_tuple)
+# tuple unpacking is also possible
+a, b, c = my_tuple
+print(a)
+print(b)
+print(c)
+
+
+
+
